de2b70d7c7d894dbab9e44d1a240fe19d692070a
krisvanneste <kris.vanneste@oma.be>
2018-07-20 15:10:57 +0000

    Re-enabled reload mechanism in __init__.py.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/db/simpledb/trunk@4735 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- __init__.py
+++ __init__.py
@@ -10,23 +10,38 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 # Make relative imports work in Python 3.x
 import os
 import sys
+import importlib
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
+try:
+    reloading
+except NameError:
+    reloading = False # means the module is being imported
+else:
+    reloading = True # means the module is being reloaded
 
-import base
-#reload(base)
+if not reloading:
+	import base
+else:
+	importlib.reload(base)
 from base import (SQLDB, SQLRecord, build_sql_query)
 
-import sqlite
-#reload(sqlite)
+if not reloading:
+	import sqlite
+else:
+	importlib.reload(sqlite)
 from sqlite import (SQLiteDB, query_sqlite_db, query_sqlite_db_generic)
 
-import mysql
-#reload(mysql)
+if not reloading:
+	import mysql
+else:
+	importlib.reload(mysql)
 if mysql.HAS_MYSQL:
 	from mysql import (MySQLDB, query_mysql_db, query_mysql_db_generic)
 
-import postgres
-#reload(postgres)
+if not reloading:
+	import postgres
+else:
+	importlib.reload(postgres)
 if postgres.HAS_POSTGRES:
 	from postgres import (PgSQLDB, query_pgsql_db, query_pgsql_db_generic)
