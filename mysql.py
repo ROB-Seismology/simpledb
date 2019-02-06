dad364397dc1db2306a399a8ba3cb3267c88ae22
krisvanneste <kris.vanneste@oma.be>
2019-02-06 14:38:34 +0000

    Added print_table argument to query_mysql_db and query_mysql_db_generic functions.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/db/simpledb/trunk@4998 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- mysql.py
+++ mysql.py
@@ -23,18 +23,16 @@ except ImportError:
 	except ImportError:
 		HAS_PYMYSQL = False
 	else:
+		from pymysql import cursors
 		HAS_PYMYSQL = True
 else:
+	from MySQLdb import cursors
 	HAS_MYSQLDB = True
 
 HAS_MYSQL = HAS_MYSQLDB or HAS_PYMYSQL
 
-if HAS_MYSQL:
-	if HAS_MYSQLDB:
-		from MySQLdb import cursors
-	else:
-		from pymysql import cursors
 
+if HAS_MYSQL:
 	class MySQLDB(SQLDB):
 		"""
 		Class representing MySQL database.
@@ -112,6 +110,7 @@ if HAS_MYSQL:
 		query,
 		port=3306,
 		verbose=False,
+		print_table=False,
 		errf=None):
 		"""
 		Generic query of MySQL database table, returning each record as a dict
@@ -131,14 +130,22 @@ if HAS_MYSQL:
 			(default: 3306)
 		:param verbose:
 			bool, whether or not to print the query (default: False)
+		:param print_table:
+			bool, whether or not to print results of query in a table
+			rather than returning records
+			(default: False)
 		:param errf:
 			file object, where to print errors
 
 		:return:
 			generator object, yielding a dictionary for each record
 		"""
+		if print_table:
+			cursor_class = cursors.Cursor
+		else:
+			cursor_class = cursors.DictCursor
 		conn = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db,
-				port=port, cursorclass=cursors.DictCursor, use_unicode=True)
+				port=port, cursorclass=cursor_class, use_unicode=True)
 		cur = conn.cursor()
 
 		if errf !=None:
@@ -148,7 +155,12 @@ if HAS_MYSQL:
 			print(query)
 
 		cur.execute(query)
-		return cur.fetchall()
+		if print_table:
+			import prettytable as pt
+			tab = pt.from_db_cursor(cur)
+			print(tab)
+		else:
+			return cur.fetchall()
 
 
 	def query_mysql_db(
@@ -165,6 +177,7 @@ if HAS_MYSQL:
 		group_clause="",
 		port=3306,
 		verbose=False,
+		print_table=False,
 		errf=None):
 		"""
 		Read table from MySQL database, returning each record as a dict
@@ -196,10 +209,9 @@ if HAS_MYSQL:
 			int, MySQL server port number
 			(default: 3306)
 		:param verbose:
-			bool, whether or not to print the query (default: False)
+		:param print_table:
 		:param errf:
-			file object, where to print errors
-			(default: None)
+			see :func:`query_mysql_db_generic`
 
 		:return:
 			generator object, yielding a dictionary for each record
@@ -207,7 +219,7 @@ if HAS_MYSQL:
 		query = build_sql_query(table_clause, column_clause, join_clause,
 						where_clause, having_clause, order_clause, group_clause)
 		return query_mysql_db_generic(db, host, user, passwd, query, port=port,
-										verbose=verbose, errf=errf)
+							verbose=verbose, print_table=print_table, errf=errf)
 
 
 
